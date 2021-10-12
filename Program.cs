0bd27e55a5b29ed35b8f458f8ed6c410fe8d0a5f
mavinii <pg.marcosoliveira@icloud.com>
2021-10-12 15:01:31 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop_lesson5_eighball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
